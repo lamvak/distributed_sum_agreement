d7508c912022645d4abfee71ceb1d7bc844565d8
Marcin Grabowski <lamvak@gmail.com>
2016-11-09 01:12:29 +0100

    Add random number generator with basic test

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      otpRand,
+      otpRand', -- preferably, would keep this one private but easiest tactical for testing in separation from real RNG
     ) where
+
+import System.Random
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+otpRand :: RandomGen g => g -> (Double, g)
+otpRand = otpRand' . random
+
+otpRand' :: RandomGen g => (Double, g) -> (Double, g)
+otpRand' (d, g) = (if d == 0 then 1 else d, g)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Hspec
+import Lib
+import System.Random
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Random number generation" $ do
+    it "generates a random number" $ do
+      let g = mkStdGen 42 in
+          fst (otpRand g) `shouldBe` 0.010663729393723398
+    it "generates from (0, 1] instead of [0, 1)" $ do
+      let g = mkStdGen 42 in
+          (fst $ otpRand' (0, g)) `shouldBe` 1
+
