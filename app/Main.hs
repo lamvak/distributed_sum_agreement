9a28080a55af7a9fc19ca8aef4a9d7000ee29be0
Marcin Grabowski <lamvak@gmail.com>
2016-11-21 03:28:31 +0100

    Simple version of optimistic otp server behavior

--- app/Main.hs
+++ app/Main.hs
@@ -6,69 +6,147 @@ import Control.Concurrent
 import Control.Distributed.Process
 import Control.Distributed.Process.Backend.SimpleLocalnet
 import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Extras (ExitReason(ExitNormal))
+import Control.Distributed.Process.Extras.Time
+import Control.Distributed.Process.ManagedProcess
 import Control.Distributed.Process.Node
 import Control.Monad
-import Data.Typeable
+import Control.Monad.Trans.Class
 import Data.Binary
+import Data.Typeable (Typeable)
+import Data.Time.Clock.POSIX
 import GHC.Generics
 import Lib
 import System.Environment (getArgs)
+import System.IO
+import System.Random (mkStdGen, setStdGen)
 
-data OtpInstruction = Ping | NoMorePings ProcessId
+now :: IO Integer
+now = do
+  time <- getPOSIXTime
+  return $ round $ time * 1000.0
+
+data OtpInputMessage = OtpInputMessage ProcessId Integer Double
   deriving (Typeable, Generic)
-instance Binary OtpInstruction
+instance Binary OtpInputMessage
 
-data OtpSlaveResponse = NoMorePingsAck
+data OtpControlMessage = DumpState | StopGenerating | Quit
   deriving (Typeable, Generic)
-instance Binary OtpSlaveResponse
+instance Binary OtpControlMessage
 
-slaveAction :: OtpInstruction -> Process ()
-slaveAction instruction = getSelfPid >>= ((flip slaveAction' instruction) . show)
+type SimpleState = (Integer, Double)
+type DuplicatesHandlingState = ([Integer], Double)
 
-slaveAction' :: String -> OtpInstruction -> Process ()
-slaveAction' selfPid Ping = say $ "PONG! (from " ++ selfPid ++ ")"
-slaveAction' selfPid (NoMorePings masterPid) = do
-  say $ "ACK for NoMorePings from " ++ selfPid
-  send masterPid NoMorePingsAck
+type StateManagerInputAction a = a -> OtpInputMessage -> Process (ProcessAction a)
+type StateManagerControlAction a = (a -> String) -> a -> OtpControlMessage -> Process (ProcessAction a)
 
-slaveLoop :: Process ()
-slaveLoop = do
-  selfNode <- getSelfNode
-  liftIO $ putStrLn . ("Starting slave " ++) . show $ selfNode
-  forever $ do
-    receiveWait [
-      match slaveAction,
-      matchAny (\_ -> return ())
-      ]
-
-remotable ['slaveLoop]
-
-spawnSlave :: NodeId -> Process ProcessId
-spawnSlave node = spawn node $ $(mkStaticClosure 'slaveLoop)
-
-master :: Backend -> [NodeId] -> Process ()
-master backend slaves = do
-  slaveProcIds <- mapM spawnSlave slaves
-  let pings = take 100 . cycle $ slaveProcIds in do
-    sequence $ map (\slavePID -> send slavePID Ping) pings
+-- State Manager: manage state of the application
+stateManagerControlAction :: StateManagerControlAction a
+stateManagerControlAction stateFormatter state DumpState = do
   selfPid <- getSelfPid
-  sequence $ flip map slaveProcIds $ \pid -> send pid $ NoMorePings selfPid
-  sequence $ flip map slaveProcIds $ \_ -> receiveWait [ match ((\_ -> return ()) :: OtpSlaveResponse -> Process()) ]
-  liftIO . putStrLn $ "done waiting for slaves - terminating"
-  -- Terminate the slaves when the master terminates (this is optional)
+  say $ "stateManagerControlAction " ++ (show selfPid) ++ ": " ++ (stateFormatter state)
+  continue state
+stateManagerControlAction _ state Quit = stop ExitNormal
+
+runStateManager :: StateManagerInputAction a -> (a -> String) -> b -> (InitHandler b a) -> Process ()
+runStateManager inputAction stateFormatter argv init = do
+  say "in runStateManager"
+  selfPid <- getSelfPid
+  serve argv init $ defaultProcess {
+    apiHandlers = [
+            handleCast $ stateManagerControlAction stateFormatter
+          , handleCast inputAction
+    ]
+  , unhandledMessagePolicy = Log
+  , timeoutHandler = \s _ -> (say $ "timeout") >> continue s
+  , shutdownHandler = \_ _ -> do
+      selfPid <- getSelfPid
+      say $ "shutting down server " ++ (show selfPid)
+      return ()
+  }
+  say $ "Server terminated: StateManager at " ++ (show selfPid)
+
+-- Optimistic State Manager - assume all messages come through, no duplicates, no reordering, no adversaries
+optimisticInputAction :: StateManagerInputAction SimpleState
+optimisticInputAction state (OtpInputMessage _ _ input) = do
+  selfPid <- getSelfPid
+  say $ "optimisticInputAction " ++ (show selfPid)
+  let i = fst state in continue (i+1, input * (fromInteger i) + (snd state))
+
+runOptimisticStateManager :: Process ()
+runOptimisticStateManager = do
+  runStateManager optimisticInputAction show () (\_ -> do
+    selfPid <- getSelfPid
+    say $ "initializing " ++ (show selfPid)
+    return $ InitOk (0, 0.0) Infinity
+    )
+
+
+-- Input Generator: generate input and send out to state managers
+inputGenerator :: IO () -> IO () -> [ProcessId] -> Process ()
+inputGenerator sendDelay iterationDelay consumers = do
+  selfPid <- getSelfPid
+  say $ "starting generator " ++ (show selfPid)
+  forever $ do
+    say $ "iteration of " ++ (show selfPid)
+    liftIO iterationDelay
+    timeStamp <- liftIO now
+    input <- liftIO otpRandIO
+    say $ (show selfPid) ++ " sending out " ++ (show input) ++
+         " at " ++ (show timeStamp) ++ " to " ++ (show consumers)
+    sequence_ $ (flip map) consumers $ \pid -> do
+        liftIO sendDelay
+        cast pid $ OtpInputMessage selfPid timeStamp input
+
+optimisticInputGenerator :: [ProcessId] -> Process ()
+optimisticInputGenerator = inputGenerator (return ()) (threadDelay 300000)
+
+remotable ['runOptimisticStateManager, 'optimisticInputGenerator]
+
+spawnOptimisticInputGenerator :: [ProcessId] -> NodeId -> Process ProcessId
+spawnOptimisticInputGenerator consumers node = spawn node $ $(mkClosure 'optimisticInputGenerator) consumers
+
+spawnOptimisticServer :: NodeId -> Process ProcessId
+spawnOptimisticServer node = spawn node $ $(mkStaticClosure 'runOptimisticStateManager)
+
+askForState :: ProcessId -> Process ()
+askForState pid = cast pid DumpState
+
+requestStop :: ProcessId -> Process ()
+requestStop pid = cast pid Quit
+
+master :: Int -> Int -> Backend -> [NodeId] -> Process ()
+master sendFor waitFor backend slaves = do
+  selfPid <- getSelfPid
+  say $ "Starting master: " ++ (show selfPid)
+  say $ "Slaves: " ++ (show slaves)
+  stateManagers <- sequence $ map spawnOptimisticServer slaves
+  say $ "State managers: " ++ (show stateManagers)
+  inputGenerators <- let spawner = spawnOptimisticInputGenerator stateManagers in
+    sequence $ map spawner slaves
+  liftIO $ threadDelay 500000
+  say "Asking state managers to dump state"
+  sequence $ map askForState stateManagers
+  liftIO $ threadDelay 2000000
+  say "done waiting for state managers to dump state; requesting state managers to stop"
+  sequence $ map requestStop stateManagers
+  liftIO $ threadDelay 2000000
+  say "done waiting for state managers to stop"
   terminateAllSlaves backend
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn $ "Application run with following args: " ++ (show args)
-  case args of
-    ["master", host, port] -> do
-      putStrLn $ "Starting master at " ++ host ++ ":" ++ port
-      backend <- initializeBackend host port (__remoteTable initRemoteTable)
-      liftIO $ threadDelay 100000
-      startMaster backend (master backend)
-    ["slave", host, port] -> do
-      putStrLn $ "Starting slave at " ++ host ++ ":" ++ port
-      backend <- initializeBackend host port (__remoteTable initRemoteTable)
-      startSlave backend
+  argv <- getArgs
+  maybe usage runApp $ parseArgs argv
+
+runApp :: RunParams -> IO ()
+runApp (RunParams mode host port seed sendFor waitFor) = do
+  setStdGen $ mkStdGen seed
+  dbgStr $ "Starting " ++ (show mode) ++ " at " ++ host ++ ":" ++ port
+  backend <- initializeBackend host port (__remoteTable initRemoteTable)
+  case mode of
+    Master -> threadDelay 500000 >> (startMaster backend $ master sendFor waitFor backend)
+    Slave -> startSlave backend
+
+dbgStr :: String -> IO ()
+dbgStr = hPutStrLn stderr
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,15 +2,84 @@ module Lib
     ( someFunc,
       otpRand,
       otpRand', -- preferably, would keep this one private but easiest tactical for testing in separation from real RNG
+      otpRandIO,
+      RunMode(Master,Slave),
+      RunParams(RunParams),
+      parseArgs,
+      usage,
     ) where
 
 import System.Random
+import Text.Heredoc
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+otpRandCorrection :: Double -> Double
+otpRandCorrection d = if d == 0 then 1 else d
 
 otpRand :: RandomGen g => g -> (Double, g)
 otpRand = otpRand' . random
 
 otpRand' :: RandomGen g => (Double, g) -> (Double, g)
-otpRand' (d, g) = (if d == 0 then 1 else d, g)
+otpRand' (d, g) = (otpRandCorrection d, g)
+
+otpRandIO :: IO Double
+otpRandIO = otpRandCorrection <$> randomIO
+
+
+-- list operations (i.e. filter out -> for rediscovery of new slaves)
+
+
+-- Commandline parameters parsing
+data RunMode = Master | Slave
+  deriving (Show)
+data RunParams = RunParams { runMode :: RunMode
+                           , host :: String
+                           , port :: String
+                           , rngSeed :: Int
+                           , sendFor :: Int
+                           , waitFor :: Int
+                           } deriving (Show)
+data IncompleteRunParams = IRP { mRunMode :: Maybe RunMode
+                               , mHost :: Maybe String
+                               , mPort :: Maybe String
+                               , mRngSeed :: Maybe Int
+                               , mSendFor :: Maybe Int
+                               , mWaitFor :: Maybe Int
+                               }
+
+parseArgs :: [String] -> Maybe RunParams
+parseArgs = parseArgs' $ IRP Nothing Nothing Nothing Nothing Nothing Nothing
+
+parseArgs' :: IncompleteRunParams -> [String] -> Maybe RunParams
+parseArgs' irp@IRP{} ("--master":h:p:params) = parseArgs' irp{mRunMode=Just Master, mHost=Just h, mPort=Just p} params
+parseArgs' irp@(IRP{}) ("--slave":h:p:params) = parseArgs' irp{mRunMode=Just Slave, mHost=Just h, mPort=Just p} params
+parseArgs' irp@(IRP{}) ("--send-for":sendForStr:params) = parseArgs' irp{mSendFor=Just (read sendForStr)} params
+parseArgs' irp@(IRP{}) ("--wait-for":waitForStr:params) = parseArgs' irp{mWaitFor=Just (read waitForStr)} params
+parseArgs' irp@(IRP{}) ("--with-seed":rngSeedStr:params) = parseArgs' irp{mRngSeed=Just (read rngSeedStr)} params
+parseArgs' irp [] = argsFrom irp
+parseArgs' _ _ = Nothing
+
+argsFrom :: IncompleteRunParams -> Maybe RunParams
+argsFrom (IRP runMode host port rngSeed sendFor waitFor) = do
+  mode <- runMode
+  h <- host
+  p <- port
+  seed <- rngSeed
+  case mode of
+    Master -> do
+      sF <- sendFor
+      wF <- waitFor
+      return $ RunParams mode h p seed sF wF
+    Slave -> return $ RunParams mode h p seed (-1) (-1)
+
+usage :: IO ()
+usage = putStrLn $ "Usage: stack exec otp-exe -- <options>\n" ++
+                 "  where options are:\n" ++
+                 "        --master <host> <port>\n" ++
+                 "        --slave  <host> <port>\n" ++
+                 "        --send-for <send for period> *\n" ++
+                 "        --wait-for <wait for period> *\n" ++
+                 "        --with-seed <RNG seed>\n" ++
+                 "  * not required for slave\n"
